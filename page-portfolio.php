912e11e35d5aaca42ebd149003c99f8eda572357
Gott50 <xxGott50xx@gmail.com>
2018-09-06 21:47:39 +0200

    add /portfolio

---
+++ page-portfolio.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Jorizon
+ */
+
+get_header();
+?>
+    <div class="hero portraits"
+        style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/johannes-lesser-portfolio-effect.png), url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/Lesser-Portrait-Hero.jpg)">
+        <div class="portfolio_text_holder">
+            <h1 class="portfoliotitle">Ich mache das für dich</h1>
+            <h1 class="subtitle_portfolio">Ich bin ein reisefreudiger Fotograf aus München.</h1>
+            <h1 class="subtitle_portfolio sub_subtitle">Bei Landschaften habe ich mich auf Print &amp; Stock Fotos spezialisiert. Bei Portraits bin ich für Ihre Buchungen da.</h1>
+        </div>
+    </div>
+    <div class="column one portfolio">
+        <div class="leftholder">
+            <h1 class="title2">Portraits</h1><a href="#" class="title3 m_left landscapes">Book me</a></div>
+        <div class="portfolio_pics_holder">
+            <div class="herotwo first portraitfirst"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            <div class="mid_hero_holder"
+                style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);">
+                <div class="herotwo secondpic portraitsecond"></div>
+            </div>
+            <div class="herotwo thirdpic portraitthird"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+    <div class="column one portfolio columntwo">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="herotwo fourthpic portraitfourth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            <div class="herotwo fifthpic portraitfifth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+    <div class="column one portfolio thirdcolumn">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="mid_hero_holder third_column_cta">
+                <div class="herotwo secondpic portraitsixth"
+                     style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            </div>
+            <div class="herotwo thirdpic portraitseventh"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+    <div class="column one portfolio thirdcolumn nodistance">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="herotwo first portraiteightth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            <div class="mid_hero_holder">
+                <div class="herotwo secondpic portraitnineth"
+                     style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            </div>
+            <div class="herotwo thirdpic portraittenth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+    <div class="column one portfolio columntwo">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="herotwo fourthpic portraiteleventh"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            <div class="herotwo fifthpic portraittwelveth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+    <div class="column one portfolio thirdcolumn">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="mid_hero_holder third_column_cta">
+                <div class="herotwo secondpic portraitthirteenth"
+                     style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+            </div>
+            <div class="herotwo thirdpic portraitfourteenth"
+                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_8533-1.jpg);"></div>
+        </div>
+    </div>
+<?php
+get_footer();
