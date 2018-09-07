340d793d11d0566adbd78cae802fc076f112389e
Gott50 <xxGott50xx@gmail.com>
2018-09-07 13:16:59 +0200

    use dynamic Link to home_url( '/' )

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 	        ?>
             <div class="socialmedia"><a href="https://www.instagram.com/jorizon/?hl=de" target="_blank" class="sociallogo"
                 style="    background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/Asset-17.svg)"></a></div>
-            <div class="socialmedia leftbar"><a href="index.html" class="sociallogo homelogo portfoliologo"
+            <div class="socialmedia leftbar"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="sociallogo homelogo portfoliologo"
                 style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/insta-logo-black.svg);"></a></div>
         </div>
         <div class="imprint two">
