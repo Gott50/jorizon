c98c921be40eb10f80578ebd21039af7279b2202
Gott50 <xxGott50xx@gmail.com>
2018-09-17 12:27:02 +0200

    import single.php Style form PhotoBlog_s

--- single.php
+++ single.php
@@ -7,31 +7,74 @@
  * @package Jorizon
  */
 
-get_header();
-?>
+get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <h2 class="screen-reader-text"><?php echo __( 'Posts navigation' ) ?></h2>
+<?php $in_same_term = false;
+$prevPost           = get_previous_post( $in_same_term );
+if ( $prevPost ): ?>
+    <div class="post-navigation nav-previous">
+		<?php $angle_left =  "<" ?>
+		<?php $prevthumbnail = get_the_post_thumbnail( $prevPost->ID, array( 300, 300 ) ); ?>
+		<?php previous_post_link( '%link',
+			"
+                <div class='posts-preview'>
+                    $prevthumbnail  <p>%title</p>
+                </div> $angle_left ", $in_same_term ); ?>
+    </div>
+<?php endif ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<?php $nextPost = get_next_post( $in_same_term );
+if ( $nextPost ): ?>
+    <div class="post-navigation nav-next">
+		<?php $angle_right =  ">"?>
+		<?php $nextthumbnail = get_the_post_thumbnail( $nextPost->ID, array( 300, 300 ) ); ?>
+		<?php next_post_link( '%link',
+			"$angle_right 
+                <div class='posts-preview'>
+                    $nextthumbnail  <p>%title</p>
+                </div> ", $in_same_term ); ?>
+    </div>
+<?php endif ?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
 
-			the_post_navigation();
+			<?php
+			while ( have_posts() ) : the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				get_template_part( 'template-parts/content', get_post_type() );
 
-		endwhile; // End of the loop.
-		?>
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :?>
+                    <div onclick="toggleComments(this)" id="site-comments" class="single-post-comments post-attachment">
+                        <button aria-expanded="false">
+							<?php
+							$number =  get_comments_number();
+							echo sprintf( _n( '%s Comment', '%s Comments', $number ), number_format_i18n( $number ) )
+							?> ...
+                        </button>
+                        <ul>
+							<?php comments_template(); ?>
+                        </ul>
+                    </div>
+				<?php
+				endif;
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			endwhile; // End of the loop.
+			?>
 
+        </main><!-- #main -->
+    </div><!-- #primary -->
+    <script>
+        function toggleComments(container) {
+            if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+                container.className = container.className.replace( ' toggled', '' );
+            } else {
+                container.className += ' toggled';
+            }
+        }
+    </script>
 <?php
 get_sidebar();
 get_footer();
