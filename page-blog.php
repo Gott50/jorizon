f29ee86479e8c530094707a7a07b9c6fecdcd194
Gott50 <xxGott50xx@gmail.com>
2018-09-20 13:56:37 +0200

    add background-image

--- page-blog.php
+++ page-blog.php
@@ -11,7 +11,8 @@ get_header();
         <div class="leftholder">
             <h1 class="title2">Blog</h1><a href="#" class="title3 m_left landscapes">Book me</a></div>
         <div class="portfolio_pics_holder firstholder">
-            <div class="herotwo first blogfirst"></div>
+            <div class="herotwo first blogfirst"
+                style=" background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/JSL01712-1.jpg);"></div>
             <div class="title2 most_recent_badge">Most recent</div>
             <h1 class="blogtitle1 firsttext">Landschafts Meerphotos</h1>
             <h1 class="subtitle_blog firsttext">Wie man solche Bilder knipst</h1>
@@ -23,7 +24,8 @@ get_header();
         <div class="portfolio_pics_holder">
             <div class="div-block">
                 <div class="mid_hero_holder_portfolio">
-                    <div class="herotwo first landscapefirst"></div>
+                    <div class="herotwo first landscapefirst"
+                         style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                     <div class="blog_text_hold">
                         <h1 class="blogtitle1">Landschafts Meerphotos</h1>
                         <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
@@ -36,11 +38,13 @@ get_header();
                         <h1 class="subtitle_blog rightaligned">Wie man solche Bilder knipst</h1>
                         <h1 class="blogtime">1 day ago</h1>
                         <h1 class="subtitle_blog sub_subtitle rightaligned">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
-                    <div class="herotwo first landscapebottom"></div>
+                    <div class="herotwo first landscapebottom"
+                         style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                 </div>
             </div>
             <div class="right_hero_holder_portfolio">
-                <div class="herotwo thirdpic blogthird"></div>
+                <div class="herotwo thirdpic blogthird"
+                     style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                 <div class="blog_text_hold secondblog">
                     <h1 class="blogtitle1">Landschafts Meerphotos</h1>
                     <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
@@ -54,7 +58,8 @@ get_header();
         <div class="portfolio_pics_holder">
             <div class="div-block">
                 <div class="mid_hero_holder_portfolio secondcolumn">
-                    <div class="herotwo first landscapefirst blogfourth"></div>
+                    <div class="herotwo first landscapefirst blogfourth"
+                         style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                     <div class="blog_text_hold">
                         <h1 class="blogtitle1">Landschafts Meerphotos</h1>
                         <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
@@ -67,11 +72,13 @@ get_header();
                         <h1 class="subtitle_blog rightaligned">Wie man solche Bilder knipst</h1>
                         <h1 class="blogtime">1 day ago</h1>
                         <h1 class="subtitle_blog sub_subtitle rightaligned">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
-                    <div class="herotwo first landscapebottom blogsixth"></div>
+                    <div class="herotwo first landscapebottom blogsixth"
+                         style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                 </div>
             </div>
             <div class="right_hero_holder_portfolio">
-                <div class="herotwo thirdpic blogthird blogfifth"></div>
+                <div class="herotwo thirdpic blogthird blogfifth"
+                     style="background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/IMG_9019.jpg);"></div>
                 <div class="blog_text_hold secondblog">
                     <h1 class="blogtitle1">Landschafts Meerphotos</h1>
                     <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
