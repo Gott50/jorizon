e3b156eeec1a51df98a9420b1b0e4f71f76fdd9a
Gott50 <xxGott50xx@gmail.com>
2018-09-20 13:49:56 +0200

    add /blog

---
+++ page-blog.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying /blog
+ *
+ * @package Jorizon
+ */
+
+get_header();
+?>
+    <div class="column one blog">
+        <div class="leftholder">
+            <h1 class="title2">Blog</h1><a href="#" class="title3 m_left landscapes">Book me</a></div>
+        <div class="portfolio_pics_holder firstholder">
+            <div class="herotwo first blogfirst"></div>
+            <div class="title2 most_recent_badge">Most recent</div>
+            <h1 class="blogtitle1 firsttext">Landschafts Meerphotos</h1>
+            <h1 class="subtitle_blog firsttext">Wie man solche Bilder knipst</h1>
+            <h1 class="blogtime">1 day ago</h1>
+            <h1 class="subtitle_blog sub_subtitle firsttext">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button first_more_button w-button">Mehr lesen</a></div>
+    </div>
+    <div class="column one blog">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="div-block">
+                <div class="mid_hero_holder_portfolio">
+                    <div class="herotwo first landscapefirst"></div>
+                    <div class="blog_text_hold">
+                        <h1 class="blogtitle1">Landschafts Meerphotos</h1>
+                        <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
+                        <h1 class="blogtime">1 day ago</h1>
+                        <h1 class="subtitle_blog sub_subtitle">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
+                </div>
+                <div class="mid_hero_holder_portfolio bottomholder">
+                    <div class="blog_text_hold right_aligned_holder">
+                        <h1 class="blogtitle1 rightaligned">Landschafts Meerphotos</h1>
+                        <h1 class="subtitle_blog rightaligned">Wie man solche Bilder knipst</h1>
+                        <h1 class="blogtime">1 day ago</h1>
+                        <h1 class="subtitle_blog sub_subtitle rightaligned">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
+                    <div class="herotwo first landscapebottom"></div>
+                </div>
+            </div>
+            <div class="right_hero_holder_portfolio">
+                <div class="herotwo thirdpic blogthird"></div>
+                <div class="blog_text_hold secondblog">
+                    <h1 class="blogtitle1">Landschafts Meerphotos</h1>
+                    <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
+                    <h1 class="blogtime">1 day ago</h1>
+                    <h1 class="subtitle_blog sub_subtitle">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="column one blog">
+        <div class="leftholder"></div>
+        <div class="portfolio_pics_holder">
+            <div class="div-block">
+                <div class="mid_hero_holder_portfolio secondcolumn">
+                    <div class="herotwo first landscapefirst blogfourth"></div>
+                    <div class="blog_text_hold">
+                        <h1 class="blogtitle1">Landschafts Meerphotos</h1>
+                        <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
+                        <h1 class="blogtime">1 day ago</h1>
+                        <h1 class="subtitle_blog sub_subtitle">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
+                </div>
+                <div class="mid_hero_holder_portfolio bottomholder">
+                    <div class="blog_text_hold right_aligned_holder">
+                        <h1 class="blogtitle1 rightaligned">Landschafts Meerphotos</h1>
+                        <h1 class="subtitle_blog rightaligned">Wie man solche Bilder knipst</h1>
+                        <h1 class="blogtime">1 day ago</h1>
+                        <h1 class="subtitle_blog sub_subtitle rightaligned">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div>
+                    <div class="herotwo first landscapebottom blogsixth"></div>
+                </div>
+            </div>
+            <div class="right_hero_holder_portfolio">
+                <div class="herotwo thirdpic blogthird blogfifth"></div>
+                <div class="blog_text_hold secondblog">
+                    <h1 class="blogtitle1">Landschafts Meerphotos</h1>
+                    <h1 class="subtitle_blog">Wie man solche Bilder knipst</h1>
+                    <h1 class="blogtime">1 day ago</h1>
+                    <h1 class="subtitle_blog sub_subtitle">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button w-button">Mehr lesen</a></div><a href="#" class="title2 load_more_button w-button">⇩ Mehr Beiträge laden</a></div>
+        </div>
+    </div>
+<?php
+get_footer();
