98e7585e0f8dcb5eb9bb7a3ed9cc077183cb086a
Gott50 <xxGott50xx@gmail.com>
2018-09-21 12:51:15 +0200

    add Fonts

--- header.php
+++ header.php
@@ -27,6 +27,10 @@
     <meta name="msapplication-config" content="<?php bloginfo( 'template_url' ); ?>/assets/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
+    <script type="text/javascript">WebFont.load({  google: {    families: ["Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic"]  }});</script>
+    <script src="https://use.typekit.net/vzs8ift.js" type="text/javascript"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<?php wp_head(); ?>
 </head>
 
