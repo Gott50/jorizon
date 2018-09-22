5e42139599c8f783b134df4653aa15f99b1337a4
Gott50 <xxGott50xx@gmail.com>
2018-09-22 20:53:10 +0200

    add SVG for .post-navigation

--- single.php
+++ single.php
@@ -10,27 +10,26 @@
 get_header(); ?>
 
     <h2 class="screen-reader-text"><?php echo __( 'Posts navigation' ) ?></h2>
+<?php $angle = "<svg class='icon icon-angle-down' xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 113.69 52.07\"><title>Asset 187</title><g id=\"Layer_2\" data-name=\"Layer 2\"><g id=\"Layer_1-2\" data-name=\"Layer 1\"><path d=\"M62.13,46.79h0l37-37,3-3c3.85-3.85,7.7-4.23,11.56-2.64-5.29-4.38-12.77-6.73-19.88.38l-37,37-37-37C12.77-2.58,5.29-.23,0,4.15,3.85,2.56,7.71,2.94,11.56,6.8l3,3,37,37,5.29,5.28Z\"/></g></g></svg>" ?>
 <?php $in_same_term = false;
 $prevPost           = get_previous_post( $in_same_term );
 if ( $prevPost ): ?>
     <div class="post-navigation nav-previous">
-		<?php $angle_left =  "<" ?>
 		<?php $prevthumbnail = get_the_post_thumbnail( $prevPost->ID, array( 300, 300 ) ); ?>
 		<?php previous_post_link( '%link',
 			"
                 <div class='posts-preview'>
                     $prevthumbnail  <p>%title</p>
-                </div> $angle_left ", $in_same_term ); ?>
+                </div> $angle ", $in_same_term ); ?>
     </div>
 <?php endif ?>
 
 <?php $nextPost = get_next_post( $in_same_term );
 if ( $nextPost ): ?>
     <div class="post-navigation nav-next">
-		<?php $angle_right =  ">"?>
 		<?php $nextthumbnail = get_the_post_thumbnail( $nextPost->ID, array( 300, 300 ) ); ?>
 		<?php next_post_link( '%link',
-			"$angle_right 
+			"$angle 
                 <div class='posts-preview'>
                     $nextthumbnail  <p>%title</p>
                 </div> ", $in_same_term ); ?>
