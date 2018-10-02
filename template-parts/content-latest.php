8b276013a5b0968ec9def46ffa3a83c97c120519
Gott50 <xxGott50xx@gmail.com>
2018-10-02 21:24:22 +0200

    use Design for content-latest.php

--- template-parts/content-latest.php
+++ template-parts/content-latest.php
@@ -9,69 +9,38 @@
 
 ?>
 
+<style>
+    .attachment-post-thumbnail {
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="column one blog">
         <header class="page-header leftholder">
-	        <?php
-	        the_archive_title( '<h1 class="page-title title2">', '</h1>' );
-	        the_archive_description( '<div class="archive-description">', '</div>' );
-	        ?>
+			<?php
+			the_archive_title( '<h1 class="page-title title2">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
             <a href="#" class="title3 m_left landscapes">Book me</a>
         </header> <!-- .page-header -->
         <div class="portfolio_pics_holder firstholder">
-            <div class="herotwo first blogfirst"
-                 style=" background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/assets/JSL01712-1.jpg);"></div>
+            <div class="herotwo first blogfirst">
+				<?php jorizon_post_thumbnail(); ?>
+            </div>
+
             <div class="title2 most_recent_badge">Most recent</div>
-            <h1 class="blogtitle1 firsttext">Landschafts Meerphotos</h1>
-            <h1 class="subtitle_blog firsttext">Wie man solche Bilder knipst</h1>
-            <h1 class="blogtime">1 day ago</h1>
-            <h1 class="subtitle_blog sub_subtitle firsttext">Als Teil meines Berufs besuche ich nun schon seit beinahe zehn Jahren Veranstaltungen von Apple. Das Event am vergangenen Mittwoch im ...</h1><a href="#" class="title2 read_more_button first_more_button w-button">Mehr lesen</a></div>
-    </div>
-
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				jorizon_posted_on();
-				jorizon_posted_by();
+			<?php
+			the_title( '<h2 class="entry-title blogtitle1 firsttext">', '</h2>' );
+			if ( 'post' === get_post_type() ) :
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php jorizon_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jorizon' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jorizon' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php jorizon_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+                <div class="entry-meta blogtime">
+					<?php jorizon_posted_on(); ?>
+                </div><!-- .entry-meta -->
+			<?php endif; ?>
+            <h4 class="subtitle_blog sub_subtitle firsttext"><?php the_excerpt()?></h4>
+            <a href="<?php the_permalink() ?>" rel="bookmark"
+               class="title2 read_more_button first_more_button w-button">Mehr lesen</a></div>
+    </div>
 </article><!-- #post-<?php the_ID(); ?> -->
