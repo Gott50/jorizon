c50829a1ce572db7fd1efc75e2a351d47eeead63
Gott50 <xxGott50xx@gmail.com>
2018-10-03 09:57:32 +0200

    rename content-second.php to content-2.php

--- home.php
+++ home.php
@@ -34,7 +34,7 @@ get_header();
 					if (is_latest()):
                         get_template_part( 'template-parts/content', 'latest' );
 					elseif (is_nth(2)):
-                        get_template_part( 'template-parts/content', 'second' );
+                        get_template_part( 'template-parts/content', '2' );
 					else:
                         get_template_part( 'template-parts/content', get_post_type() );
 					endif;
